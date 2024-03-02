0db858de0b77a361109a1ec53c600ba99239d1e1
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2024-03-02 19:22:23 +0100

    Initial Commit

---
+++ EmailSwiftUI/EmailSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmailSwiftUIApp.swift
+//  EmailSwiftUI
+//
+//  Created by LAANAYA Abderrazak on 2/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmailSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
